02f2c0818747d2ee3ff01830f98d44b7ec90977f
nkleinbaer <47011214+nkleinbaer@users.noreply.github.com>
2021-03-03 12:54:27 -0700

    Update installfastai2.sh
    replaced pip w/ conda

--- Samples/fastai2/installfastai2.sh
+++ Samples/fastai2/installfastai2.sh
@@ -6,6 +6,5 @@ git clone https://github.com/fastai/fastbook.git
 git clone https://github.com/fastai/course-v4.git
 conda create -y -n fastai2 python=3.7
 conda activate fastai2
-pip install -r fastbook/requirements.txt
-pip install -Uqq fastbook
+conda install fastai fastbook
 EOF
