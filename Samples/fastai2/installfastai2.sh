5d5b6c44a287cd33666606425a5d9d351c5d98de
nkleinbaer <nicholaskleinbaer@gmail.com>
2021-03-05 10:44:17 -0700

    Revert "Update installfastai2.sh"
    This reverts commit 02f2c0818747d2ee3ff01830f98d44b7ec90977f.

--- Samples/fastai2/installfastai2.sh
+++ Samples/fastai2/installfastai2.sh
@@ -6,5 +6,6 @@ git clone https://github.com/fastai/fastbook.git
 git clone https://github.com/fastai/course-v4.git
 conda create -y -n fastai2 python=3.7
 conda activate fastai2
-conda install fastai fastbook
+pip install -r fastbook/requirements.txt
+pip install -Uqq fastbook
 EOF
